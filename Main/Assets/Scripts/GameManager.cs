471889ec242b53f4e0499fc4a078c4542bab84d9
upq4103 <56025364+upq4103@users.noreply.github.com>
2019-10-02 10:11:50 +0200

    +Main Menu
    Added main menu scene and contents

---
+++ Main/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void CompleteLevel()
+    {
+        Invoke("NextLevel", 2f);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Main/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        FindObjectOfType<GameManager>().NextLevel();
+    }
+    public void QuitGame ()
+
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
