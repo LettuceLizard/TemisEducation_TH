c01fab247d56e1ab74ba8f5dc177ccd59d502d07
upq4103 <56025364+upq4103@users.noreply.github.com>
2019-10-10 14:17:59 +0200

    +video

---
+++ Main/Assets/Videoplayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class Videoplayer : MonoBehaviour
+{
+    private VideoPlayer Video;
+    void Start()
+    {
+        Video = GetComponent<VideoPlayer>();
+        Video.enabled = false;
+    }
+
+    void Update()
+    {
+        Video.enabled = true;        
+    }
+}
