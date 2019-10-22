b343fdf9dac41fb6baccc2bcf86c94887b08dfcf
upq4103 <56025364+upq4103@users.noreply.github.com>
2019-10-22 09:59:45 +0200

    werwersc

--- Main/Assets/completelevel.cs
+++ Main/Assets/completelevel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimerNextScene : MonoBehaviour
+public class completelevel : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
