923aeb6f4dbb4a0effafdc4009b8027b8cc9eb93
upq4103 <56025364+upq4103@users.noreply.github.com>
2019-10-29 09:41:21 +0100

    cool

--- Main/Assets/Scripts/LV 1/PlayerInput.cs
+++ Main/Assets/Scripts/LV 1/PlayerInput.cs
@@ -6,7 +6,7 @@ public class PlayerInput : MonoBehaviour
     private string playerAnswer;
     public GameObject inputField;
     public Text textDisplay;
-
+    
     public void Answer(string rightAnswer)
     {
         playerAnswer = inputField.GetComponent<Text>().text;
