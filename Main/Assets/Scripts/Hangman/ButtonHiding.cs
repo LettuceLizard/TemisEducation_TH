965055c4208b2543079ab9f1ce60b9cf5b15bb9f
upq4103 <56025364+upq4103@users.noreply.github.com>
2019-11-07 16:24:41 +0100

    lmlknln

--- Main/Assets/Scripts/Hangman/ButtonHiding.cs
+++ Main/Assets/Scripts/Hangman/ButtonHiding.cs
@@ -3,13 +3,16 @@
 public class ButtonHiding : MonoBehaviour
 {
     private GameObject button;
+    int index;
+    char indexAsChar;
+    string indexAsString;
+    
     public void HideButton()
     {
-        int index = PlayerPrefs.GetInt("buttonIndex") - 32;
-        char indexAsChar = System.Convert.ToChar(index);
-        string indexAsString = System.Convert.ToString(indexAsChar);
+        index = PlayerPrefs.GetInt("buttonIndex") - 32;
+        indexAsChar = System.Convert.ToChar(index);
+        indexAsString = System.Convert.ToString(indexAsChar);
 
-        button = GameObject.Find(indexAsString);
-        button.gameObject.SetActive(false);
+        GameObject.Find(indexAsString).SetActive(false);
     }
 }
--- Main/Assets/Scripts/Hangman/HangmanManager.cs
+++ Main/Assets/Scripts/Hangman/HangmanManager.cs
@@ -1,110 +1,99 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections.Generic;
+//---------------------------------------------------//
+//-------------Använd en dubbel for-loop-------------//
+//---------------------------------------------------//
 public class HangmanManager : MonoBehaviour
 {
-    public GameObject letter;
-    private GameObject cen;
-    private string wordToGuess = "";
+    public string wordToGuess;
     private int lengthOfWordToGuess;
-    char [] lettersToGuess;
-    bool [] lettersGuessed;
-    
+    private GameObject center;
+    public GameObject pad;
+    Vector3 newPosition;
+    private int nbletters;
+    private char letterPressedAsChar;
+    int n = 0;
+    int j = 0;
+    bool wordFound;
+    int truelength;
+    List<char> word = new List<char>();
+    int temp;
+
     void Start()
     {
-        cen = GameObject.Find ("centerOfScreen");
-        initGame ();
-        initLetters ();
+        center = GameObject.Find("centerOfScreen");
+        SetupGame();
+        SetupLayout();
     }
 
-    void initLetters()
+    void SetupGame()
     {
-        int nbletters = lengthOfWordToGuess;
+        lengthOfWordToGuess = wordToGuess.Length;
+        wordToGuess = wordToGuess.ToUpper();
+
+        for (int i = 0; i < lengthOfWordToGuess; i++)
+        {
+            word.Add(wordToGuess[i]);               
+            Debug.Log(word.Count);
+            Debug.Log(word[i]);
+            //for (int k = 0; k < lengthOfWordToGuess; k++)
+            //{
+                
+            //}
+        }
+    }
+
+    void SetupLayout()
+    {
+        nbletters = lengthOfWordToGuess;
  
         for (int i = 0; i < nbletters; i++) 
         {
-            Vector3 newPosition;
-            newPosition = new Vector3 (cen.transform.position.x + (50+((i-nbletters/2.0f) *100)), cen.transform.position.y, cen.transform.position.z);
-            GameObject l = (GameObject)Instantiate (letter, newPosition, Quaternion.identity);
+            newPosition = new Vector3 (center.transform.position.x + (((i-nbletters/2.0f) *75)), center.transform.position.y, center.transform.position.z);
+            GameObject l = (GameObject)Instantiate (pad, newPosition, Quaternion.identity);
             l.name = "letter" + (i + 1);
             l.transform.SetParent(GameObject.Find ("word").transform);
+
+            if (wordToGuess[i] == ' ')
+            {
+                GameObject.Find("letter" + (i + 1)).SetActive(false);
+            }
         }
     }
-
-    void initGame()
-    {
-        wordToGuess = "Treasure";
-        lengthOfWordToGuess = wordToGuess.Length;
-        wordToGuess = wordToGuess.ToUpper ();
-        lettersToGuess = new char [lengthOfWordToGuess];
-        lettersGuessed = new bool [lengthOfWordToGuess];
-        lettersToGuess = wordToGuess.ToCharArray ();
-    }
-
-    int n = 0;
-    int j = 0;
-    private GameObject obj;
-    string objName;
-    public GameObject keys;
-    public GameObject word;
-    public GameObject gameOver;
-    public GameObject gameWon;
 
     public void Input(int letterPressedAsInt)
     {
+        wordFound = false;
         PlayerPrefs.SetInt("buttonIndex", letterPressedAsInt);
-        char letterPressed = System.Convert.ToChar(letterPressedAsInt);
         FindObjectOfType<ButtonHiding>().HideButton();
+        letterPressedAsChar = System.Convert.ToChar(letterPressedAsInt);
 
-        if (letterPressedAsInt >= 97 && letterPressed <= 122)
+        for (int i=0; i < lengthOfWordToGuess; i++)
         {
-            if (letterPressedAsInt != 116 && letterPressedAsInt != 114 && letterPressedAsInt != 101 && letterPressedAsInt != 97 && letterPressedAsInt != 115 && letterPressedAsInt != 117)
+            letterPressedAsChar = System.Char.ToUpper(letterPressedAsChar);
+            if (wordToGuess[i] == letterPressedAsChar)
             {
-                objName = "Hangman" + n;
-                obj = GameObject.Find(objName);
-                obj.SetActive(false);
+                GameObject.Find("letter" + (i + 1)).GetComponent<Text>().text = letterPressedAsChar.ToString();
+                wordFound = true;
                 n++;
             }
-
-            if (n >= 11)
-            {
-                keys.SetActive(false);
-                gameOver.SetActive(true);
-                word.SetActive(false);
-                Invoke("ReloadScene", 3f);
-            }
-            
-            for (int i=0; i < lengthOfWordToGuess; i++)
-            {
-                if (!lettersGuessed [i])
-                {
-                    letterPressed = System.Char.ToUpper (letterPressed);
-        
-                    if (lettersToGuess [i] == letterPressed)
-                    {
-                        lettersGuessed [i] = true;
-                        GameObject.Find("letter"+(i+1)).GetComponent<Text>().text = letterPressed.ToString();
-                        j++;
-                    }
-
-                    if (j >= 8)
-                    {
-                        gameWon.SetActive(true);
-                        word.SetActive(false);
-                        Invoke("NextLevel", 3f);
-                    }
-                }
-            }
         }
-    }
 
-    void ReloadScene()
-    {
-        FindObjectOfType<GameManager>().ReloadScene();
-    }
+        if (!wordFound)
+        {
+            GameObject.Find("Hangman" + j).GetComponent<Image>().enabled = false;
+            j++;
+        }
 
-    void NextLevel()
-    {
-        FindObjectOfType<GameManager>().NextLevel();
+        if (j >= 10)
+        {
+            GameObject.Find("GameOver").SetActive(true);
+        }
+
+        if (n == truelength)
+        {
+
+        }
     }
 }
